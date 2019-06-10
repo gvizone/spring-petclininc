9b1d13da805324c6effcc579725f21b673ab7b83
Gabriel Vizone <gabriel.vizone@gmail.com>
2019-06-10 09:24:40 -0300

    Added POJOs

---
+++ src/main/java/br/com/vizone/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package br.com.vizone.springpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/br/com/vizone/springpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package br.com.vizone.springpetclinic.model;
+
+public class Vet extends Person {
+
+}
