447faec718382b1f3a8b1333dddbb4945637997d
Gabriel Vizone <gabriel.vizone@gmail.com>
2019-06-18 10:43:52 -0300

    Added visits, updates owner with pet relationship.

--- pet-clinic-data/src/main/java/br/com/vizone/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/br/com/vizone/springpetclinic/model/Owner.java
@@ -1,6 +1,15 @@
 package br.com.vizone.springpetclinic.model;
 
-public class Owner extends Person {
+import java.util.Set;
 
-    private static final long serialVersionUID = 1L;
+public class Owner extends Person {
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
