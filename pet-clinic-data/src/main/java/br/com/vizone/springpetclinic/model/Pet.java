7489a314d11d760dee838a1351476f99c8e2f74d
Gabriel Vizone <gabriel.vizone@gmail.com>
2019-06-18 15:12:14 -0300

    Added Pets and Contact info to owners on startup.

--- pet-clinic-data/src/main/java/br/com/vizone/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/br/com/vizone/springpetclinic/model/Pet.java
@@ -3,10 +3,18 @@ package br.com.vizone.springpetclinic.model;
 import java.time.LocalDate;
 
 public class Pet extends BaseEntity {
-    private static final long serialVersionUID = 1L;
+    private String name;
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public PetType getPetType() {
         return petType;
