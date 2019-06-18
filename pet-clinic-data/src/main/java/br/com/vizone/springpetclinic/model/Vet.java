380ace87df80d4df2417cbba6704d339d278fd01
Gabriel Vizone <gabriel.vizone@gmail.com>
2019-06-18 10:56:16 -0300

    Added specialities, added to vet.

--- pet-clinic-data/src/main/java/br/com/vizone/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/br/com/vizone/springpetclinic/model/Vet.java
@@ -1,7 +1,15 @@
 package br.com.vizone.springpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+    private Set<Speciality> specialities;
 
-    private static final long serialVersionUID = 1L;
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
 
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
