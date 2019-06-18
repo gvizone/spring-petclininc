264a4028ac871fcece5b9f306818275801a3f69b
Gabriel Vizone <gabriel.vizone@gmail.com>
2019-06-18 15:30:13 -0300

    Added Specialties Map Impl.

---
+++ pet-clinic-data/src/main/java/br/com/vizone/springpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,9 @@
+package br.com.vizone.springpetclinic.services;
+
+import br.com.vizone.springpetclinic.model.Specialty;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+
+}
