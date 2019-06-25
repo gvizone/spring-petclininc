b4fe62007b2a453f0443ebf4d3c6b04a9c943487
Gabriel Vizone <gabriel.vizone@gmail.com>
2019-06-25 14:46:57 -0300

    Updated base entity with JPA annotations.

--- pet-clinic-data/src/main/java/br/com/vizone/springpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/br/com/vizone/springpetclinic/model/BaseEntity.java
@@ -1,9 +1,16 @@
 package br.com.vizone.springpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
-    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
